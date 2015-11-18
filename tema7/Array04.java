7e22e87844acec68f13b8c3c2391e76d9fab481f
BrandConstantin <brand_constantin@hotmail.com>
2015-11-18 14:24:12 +0100

    Añadiendo mas ejercicios de array

--- tema7/Array04.java
+++ tema7/Array04.java
@@ -9,29 +9,20 @@
  * 
  *@Costy 
  */
-import java.util.Scanner;
 
 public class Array04 {
   public static void main(String[] args) {
-    
-    Scanner num = new Scanner(System.in);
     
     int[] numero = new int[20];
     int[] cuadrado = new int[20];
     int[] cubo = new int[20];
     
+    System.out.println("NUMEROS:----CUADRADO:----CUBO");
     for (int i = 0; i < numero.length; i++) {
       numero[i] = (int)(Math.random() * 101);
-      System.out.print(numero[i] + " ");
-    }
-    
-    for (int i = 0; i < numero.length; i++) {
       cuadrado[i] = numero[i] * numero[i];
-      System.out.print(cuadrado[i] + " ");
-    }
-    for (int i = 0; i < numero.length; i++) {
       cubo[i] = numero[i] * numero[i] * numero[i];
-      System.out.print(cubo[i] + " ");
+      System.out.println(numero[i] + "\t\t" + cuadrado[i] + "\t\t" + cubo[i]);
     }
   }
 }
