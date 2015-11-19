b8f9da15e39e6ac5a19667e4433bc898a739f5c9
BrandConstantin <brand_constantin@hotmail.com>
2015-11-19 08:29:14 +0100

    Añadiendo mas ejercicios

---
+++ tema7/Array07.java
@@ -0,0 +1,44 @@
+/*
+ * Escribe un programa que genere 100 números aleatorios del 0 al 20 y que los muestre
+ * por pantalla separados por espacios. El programa pedirá entonces por teclado dos valores
+ * y a continuación cambiará todas las ocurrencias del primer valor por el segundo en
+ * la lista generada anteriormente. Los números que se han cambiado deben aparecer
+ * entrecomillados.
+ * 
+ * 
+ *@Costy 
+ */
+ 
+import java.util.Scanner;
+
+public class Array07 {
+  public static void main(String[] args) {
+    
+    Scanner num = new Scanner(System.in);
+    
+    int[] numero = new int[100];
+    
+    for (int i = 0; i < numero.length; i++) {
+      numero[i] = (int)(Math.random() * 21);
+      System.out.print(numero[i] + "  ");
+    }
+    
+    System.out.println("\nIntroduce un numero de 1 - 20 que quieres cambiar:");
+    int num1 = num.nextInt();
+    System.out.println("Introduce el numero con el cual quieres cambiar de 1- 20:");
+    int num2 = num.nextInt();
+   
+    if ((num1 > 1) && (num1 < 20) && (num2 > 1) && (num2 < 20)){
+        for (int a : numero){
+          if(a == num1){
+            System.out.print("'" + num2 + "'");
+          }else{
+            System.out.print(a);
+          }
+          System.out.print(" ");
+        }   
+    } else {
+        System.err.println("Algun numero fuera de rango!");
+    }
+  }
+}
---
+++ tema7/Array08.java
@@ -0,0 +1,43 @@
+/*
+ * Realiza un programa que pida la temperatura media que ha hecho en cada mes de un determinado año y que 
+ * muestre a continuación un diagrama de barras horizontales con esos datos. Las barras del diagrama se pueden 
+ * dibujar a base de asteriscos o cualquier otro carácter.
+ * 
+ * 
+ *@Costy 
+ */
+ 
+import java.util.Scanner;
+
+public class Array08 {
+  public static void main(String[] args) {
+    
+    Scanner num = new Scanner(System.in);
+    
+    int[] temp = new int [12]; 
+    String[] mes = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
+                      "Octubre", "Noviembre", "Diciembre"};
+    int j;
+    String verde = "\033[32m";
+    String naranja = "\033[33m";
+    String azul = "\033[34m";
+    String morado = "\033[35m";
+    String blanco = "\033[37m";
+    
+    for (int i = 0; i <= 12; i++){
+        System.out.print("Introduce la temperatura del mes:" + mes[i]);
+        temp[i] = Integer.parseInt(num.nextLine());
+    }
+    
+    System.out.println("===================================================");
+    int i;
+    
+    for (i = 0; i < 12; i++) {
+      System.out.printf(azul + "%12s " + verde + "│", mes[i]);
+      for (j = 0; j < temp[i]; j++) {
+        System.out.print(morado + "▄");
+      }
+      System.out.println(naranja + " " + temp[i] + "ºC" + blanco);
+    }
+  }
+}
