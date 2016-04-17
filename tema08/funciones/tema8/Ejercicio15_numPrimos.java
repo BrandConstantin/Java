142a05fea165de5396cb2d8b1b89701a90e064a2
BrindCo <brand_constantin@hotmail.com>
2016-04-17 12:23:21 +0200

    Actualizando ficheros

---
+++ tema08/funciones/tema8/Ejercicio15_numPrimos.java
@@ -0,0 +1,25 @@
+/*
+ * 
+ * Muestra los números primos que hay entre 1 y 1000
+ */
+package tema8;
+
+//import java.util.Scanner;
+
+/**
+ *
+ * @author BrinCo
+ */
+public class Ejercicio15_numPrimos {
+    public static void main(String[] args) {
+        //Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Mostrar números primos de 1 a 1000");
+
+        System.out.println("-------------------------------------------------------");
+
+
+        funciones.VariasMate.numPrimos();
+
+  }
+}
---
+++ tema08/funciones/tema8/Ejercicio16_capicuasDe1a99999.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema8;
+
+/**
+ *
+ * @author Milan Touch
+ */
+public class Ejercicio16_capicuasDe1a99999 {
+    
+}
