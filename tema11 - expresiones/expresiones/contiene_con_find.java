c4e17e0f6aab39b16b0ea7c5e9e5cc5496af9926
U-BRANDCONSTANTIN\brand <brand_constantin@hotmail.com>
2018-07-01 00:32:38 +0200

    expresiones y excepciones

---
+++ tema11 - expresiones/expresiones/contiene_con_find.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expresiones;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author brand
+ */
+public class contiene_con_find {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String cadena = "Este es un mensaje abc para todo el mundo";
+        
+        Pattern pat = Pattern.compile(".*abc.+");
+        Matcher mat = pat.matcher(cadena);
+        
+        if(mat.find()){
+            System.out.println("SI");
+        }else{
+            System.out.println("NO");
+        }
+    }
+    
+}
