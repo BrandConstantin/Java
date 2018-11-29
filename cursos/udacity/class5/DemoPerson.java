e033a48565dae10f406bea72189ea489f86806d3
BrandConstantin <constantin.brindu@gmail.com>
2018-11-29 17:57:46 +0100

    new more exercises from udacity

---
+++ cursos/udacity/class5/DemoPerson.java
@@ -0,0 +1,9 @@
+public class DemoPerson 
+{
+    public static void main(String[] args)
+    {
+        Person sara = new Person2("Sara", "sara.jpg", 10, 200);
+        Person chenghan = new Person2("Cheng-Han", "cheng-han.png", 300, 0);
+        Person cay = new Person2("Cay", "cay.gif", 250, 180);
+    }
+}
