996ef3f6b71e3b73b8a87551ed72c0a366f65e84
BrandConstantin <constantin.brindu@gmail.com>
2019-01-15 23:45:30 +0100

    Simulación Cliente/Servidor

--- DAM/ejemplos/21_SocketTCP/Servidor.java
+++ DAM/ejemplos/21_SocketTCP/Servidor.java
@@ -20,14 +20,18 @@ class Servidor {
 		try {
 			ServerSocket skServidor = new ServerSocket(Puerto);
 			System.out.println("Escucho el puerto " + Puerto);
+			
 			for (int nCli = 0; nCli < 3; nCli++) {
 				Socket sCliente = skServidor.accept();
 				System.out.println("Sirvo al cliente " + nCli);
+				
 				OutputStream aux = sCliente.getOutputStream();
 				DataOutputStream flujo_salida = new DataOutputStream(aux);
+				
 				flujo_salida.writeUTF("Hola cliente " + nCli);
 				sCliente.close();
 			}
+			
 			System.out.println("Ya se han atendido los 3 clientes");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
