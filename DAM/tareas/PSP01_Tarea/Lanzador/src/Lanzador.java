e31d29fc3cf655f0dc6899530b30e63f2f833bf7
BrandConstantin <constantin.brindu@gmail.com>
2019-10-23 22:20:15 +0200

    lanzador

--- DAM/tareas/PSP01_Tarea/Lanzador/src/Lanzador.java
+++ DAM/tareas/PSP01_Tarea/Lanzador/src/Lanzador.java
@@ -1,66 +1,194 @@
-/*Tercera parte. Implementa una aplicación para que lance como máximo 10 instancias de la
- aplicación 'generador'. El usuario indicará el número de instancias (respetando el máximo
-  establecido) y el número de cadenas a generar cada instancia. La aplicación se llamará
-  'lanzador'.*/
+/*
+ * Tercera parte. Implementa una aplicación para que lance como máximo 10 number
+ * de la aplicación 'array'. El usuario indicará el número de number (respetando el máximo 
+ * establecido) y el número de chains a generar cada instancia. La aplicación se llamará 'lanzador'.
+ */
+package lanzador;
 
-import java.util.Scanner;
-import java.lang.Process;
-import java.lang.Runtime;
+import java.io.*;
+import java.util.*;
 
+/**
+ *
+ * @author brand
+ */
 public class Lanzador {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
+	//máximo de instancias.
+	final static int MAXINST = 10;
+	//Mínimo constante tanto para number como chains.
+	final static int MININSTCAD = 1;
 
-        int number = 12;
-        int cadenas;
+	/**
+	 * @param number número de number
+	 * @param chains número de chains
+	 */
+	public static void lanzador(int number, int chains) {
+		while (number > 0) {
+			try {
+				// Se lanza el ejecutable.
+				Process p = Runtime.getRuntime().exec("java -jar array.jar " + chains);
+				
+				// Obtiene la salida del programa.
+				InputStream inp = p.getInputStream();
+				BufferedReader br = new BufferedReader(new InputStreamReader(inp));
+				
+				// Se lee la primera línea 
+				String line = null;
+				
+				// El bucle es recorrido mientras haya líneas
+				while ((line = br.readLine()) != null && !line.isEmpty()) {
+					System.out.println(line);
+				}//close while.
+				
+				System.out.println();
+				//Resta 1 a las instacias.
+				number--;
+			} catch (IOException ex) {
+				System.err.println("Ha ocurrido un error de Seguridad.");
+			}//close try-catch
+		}//close while. 
+	}//close Lanzador
 
-        while (number > 10 || number < 1) {
-            System.out.print("Introduzca cantidad de instancias de la aplicación array (menor de 10): ");
-            number = teclado.nextInt();
-        }
+	/**
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int number = 0;
+		int chains = 0;
+		
+		switch (args.length) {
+			case 0:
+				number = readNumber();
+				chains = readChain();
+				break;
+			case 1:
+				System.out.println("solo se paso un párametro por comando,"
+						+ " será considerado como número de number.");
+				number = readNumber(args[0]);
+				chains = readChain();
+				break;
+			case 2:
+				System.out.println("Párametros recibidos por comando");
+				number = readNumber(args[0]);
+				chains = readChain(args[1]);
+		}//close switch.
 
-        System.out.print("Indica el número de cadenas a generar: ");
-        cadenas = teclado.nextInt();
+		lanzador(number, chains);
+	}//close main.
 
-        for (int k = 0; k < number; k++) {
-            //Instancias la clase.
-            Lanzador lanzarLanzador = new Lanzador();
-            //ejecutas el metodo de la clase.
-            lanzarLanzador.Lanzador();
-        }
-    }
+	/**
+	 * lee los datos para la variable cadena y comprueba que sean válidos.
+	 *
+	 * @return
+	 */
+	public static int readNumber() {
+		Scanner sc = null;
+		int number = 0;
+		boolean check = false;
+		
+		do {
+			System.out.print("¿Cuantas instancias deseas?	");
+			try {
+				sc = new Scanner(System.in);
+				number = sc.nextInt();
 
-    public void Lanzador() {
-        String nombreFichero = "";
+				if (number >= MININSTCAD && number <= MAXINST) {
+					check = true;
+				} else {
+					System.out.println("Error, el número debe ser mayor que 0 "
+							+ "y menor o igual que 10.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Error, debe ser un número entero.");
+			}//close try-catch.    
+		} while (!check);//close do-while.
 
-        Process nuevoProceso;
+		return number;
+	}//close function.
 
-        try {
-            //Obtenemos el nombre del SO
-            String osName = System.getProperty("os.name");
+	/**
+	 * lee los datos para la variable instancia y comprueba que sean válidos, si
+	 * no son validos los vuelve a pedir.
+	 *
+	 * @param inst number
+	 * @return
+	 */
+	public static int readNumber(String inst) {
+		//en caso de ser nulo o fuera de rango se volvera a pedir el valor. 
+		int number = 0;
 
-            if (osName.toUpperCase().contains("WIN")) { //Windows
-                nombreFichero = "C:\\Users\\brand\\IdeaProjects\\Generador\\out\\artifacts\\Generador_jar\\Generador.jar";
-                nuevoProceso = Runtime.getRuntime().exec("java -jar " + nombreFichero);
-            } else {                                      //Linux
-                nombreFichero = "/home/brand/IdeaProjects/Generador/out/artifacts/Generador_jar/Generador.jar";
-                nuevoProceso = Runtime.getRuntime().exec("java -jar " + nombreFichero);
-            }
+		try {
+			number = Integer.parseInt(inst);
+		} catch (NumberFormatException e) {
+			System.out.println("Error, debe ser un número entero.");
+		}//close try-catch.  
+		
+		if (number >= MININSTCAD && number <= MAXINST) {
+			return number;
+		} else {
+			System.out.println("Error, el número debe ser mayor que 0 y "
+					+ "y menor o igual que 10.");
+			return readNumber();
+		}
+	}//close function.
 
-            nuevoProceso = Runtime.getRuntime().exec("java -jar " + nuevoProceso);
-            System.out.println(nuevoProceso);
-            System.out.println(nombreFichero);
-        } catch (SecurityException ex) {
-            System.out.println("Ha ocurrido un error de Seguridad."
-                    + "No se ha podido crear el proceso por falta de permisos.");
-        } catch (Exception ex) {
-            System.out.println("Ha ocurrido un error, descripción: "
-                    + ex.toString());
-        }
+	/**
+	 * lee los datos para la variable cadena y comprueba que sean válidos.
+	 *
+	 * @return
+	 */
+	public static int readChain() {
+		Scanner sc = null;
+		int chains = 0;
+		boolean check = false;
 
-    }
-}
+		do {
+			System.out.print("¿Cuantas cadenas deseas?	");
+			try {
+				sc = new Scanner(System.in);
+				chains = sc.nextInt();
+
+				if (chains >= MININSTCAD) {
+					check = true;
+				} else {
+					System.out.println("Error, el número debe ser mayor que 0");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Error, debe ser un número entero.");
+			}//close try-catch.
+
+		} while (!check);//close do-while.
+		return chains;
+	}//close function.
+
+	/**
+	 * lee los datos para la variable cadena y comprueba que sean válidos. si no
+	 * son validos los vuelve a pedir.
+	 *
+	 * @param cad chains
+	 * @return
+	 */
+	public static int readChain(String cad) {
+		//en caso de ser nulo o fuera de rango se volvera a pedir el valor. 
+		int chains = 0;
+
+		try {
+			chains = Integer.parseInt(cad);
+		} catch (NumberFormatException e) {
+			System.out.println("Error, debe ser un número entero.");
+		}//close try-catch.
+
+		if (chains >= MININSTCAD) {
+			return chains;
+		} else {
+			System.out.println("Error, el número debe ser mayor que 0");
+			return readChain();
+		}
+	}//close function.
+
+	void Lanzador2() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+}//close class.
