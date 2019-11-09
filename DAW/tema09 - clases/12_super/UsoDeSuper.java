117b5cc81b7e88d7d7f5359b408a3221e03b2d86
BrandConstantin <constantin.brindu@gmail.com>
2019-11-09 13:49:17 +0100

    polimorfismo, uso de super

---
+++ DAW/tema09 - clases/12_super/UsoDeSuper.java
@@ -0,0 +1,11 @@
+public class UsoDeSuper {
+    public static void main(String[] args) {
+        ClaseBase base = new ClaseBase();
+//		ClaseDerivada derivada = new ClaseDerivada();
+        ClaseDerivada derivada = new ClaseDerivada("Hola Mundo!"," desde una clase derivada");
+
+        base.imprimir();
+        System.out.println("");
+        derivada.imprimir();
+    }
+}
